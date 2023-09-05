8774aa939bde784e29ffa5d17852ac91bc7abe3c
mahdiengr <mahdi.engr@gmail.com>
2023-09-05 10:16:48 +0600

    staticfiles edit commit

--- book_shope/settings.py
+++ book_shope/settings.py
@@ -131,15 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'book_shop/static'
 
 # print('base dir ', BASE_DIR/'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR/'static',
-# ]
+STATICFILES_DIRS = [
+    'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
